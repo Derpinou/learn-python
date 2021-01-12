712d67cb6fa7234b179f231981ddeef1181d171a
Derpinou <lightcompte@gmail.com>
2021-01-12 11:06:02 +0100

    reponse aux questions

--- exec1.py
+++ exec1.py
@@ -4,3 +4,9 @@ annee = 2020
 jour = jour +1
 print("nous sommes le ", jour, "/ ", mois, "/", annee)
 #output: nous sommes le  2 /  9 / 2020
+"""
+1. Expliquez ce qu’ont fait nos instructions On déclare les variables puis on ecris dans le terminal
+2. Quelle est la fonction (instruction) utilisée dans cet exemple. Quel est son rôle ? c'est la fonction print()
+3. Quelle est la grande différence pour la gestion des variables entre le C (arduino) et Python ? nous avons pas besoin 
+de typer les variables
+"""
