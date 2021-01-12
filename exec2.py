0627dae2708abe78ac8a1618d60ca11b5a027c2f
Derpinou <lightcompte@gmail.com>
2021-01-12 11:11:47 +0100

    exec2

---
+++ exec2.py
@@ -0,0 +1,7 @@
+nb1 = 3
+nb2 = 5
+resultat = nb1 * nb2
+print(resultat)
+
+# 1. Expliquez ce qu’ont fait nos instructions nous déclarons les variables puis nous les multiplions, nous stockons le
+# resultat dans une variable puis on l'imprime
