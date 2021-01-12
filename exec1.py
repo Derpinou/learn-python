3a76779c09e663c7057c3bd7699f06f0282238e8
Derpinou <lightcompte@gmail.com>
2021-01-12 11:03:18 +0100

    add exec1

---
+++ exec1.py
@@ -0,0 +1,6 @@
+jour = 1 #(puis « entrée »)→ à faire à chaque fois
+mois = 9
+annee = 2020
+jour = jour +1
+print("nous sommes le ", jour, "/ ", mois, "/", annee)
+#output: nous sommes le  2 /  9 / 2020
